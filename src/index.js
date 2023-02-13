ca129aaf6a17ce2b79d8270a50ca216bd12976db
Danny Santino <dev@dannysantino.com>
2023-02-13 08:32:20 +0100

    feat: initial set-up for whatsapp chatbot integrated with chatgpt api

---
+++ src/index.js
@@ -0,0 +1,71 @@
+import * as dotenv from "dotenv"
+dotenv.config();
+
+import whatsappweb from "whatsapp-web.js";
+import { MongoStore } from "wwebjs-mongo";
+import mongoose from "mongoose";
+import qrcode from "qrcode-terminal"
+
+import { ChatGPTAPI } from "chatgpt";
+
+const { Client, RemoteAuth } = whatsappweb;
+
+const api = new ChatGPTAPI({ apiKey: process.env.API_KEY});
+
+let conversationId,
+    parentMessageId;
+    
+const sendMessage = async message => {
+    try {
+        console.log(`Message received from: ${message.from}`);
+
+        const response = await api.sendMessage(message.body, conversationId && {
+            conversationId,
+            parentMessageId
+        });
+        
+        conversationId = response.conversationId;
+        parentMessageId = response.parentMessageId;
+
+        console.log("ChatGPT reply received: ", response.text);
+        
+        return response;
+    } catch (err) {
+        throw new Error("Message Error", { cause: err });
+    }
+}
+
+mongoose.connect(process.env.DB_URL)
+    .then(() => {
+        const store = new MongoStore({ mongoose });
+        const whatsapp = new Client({
+            authStrategy: new RemoteAuth({
+                store,
+                backupSyncIntervalMs: 300000
+            })
+        })
+
+        whatsapp.on("remote_session_saved", () => console.log("Session saved"));
+
+        whatsapp.on("qr", qr => qrcode.generate(qr, { small: true }));
+
+        whatsapp.on("ready", () => console.log("WhatsApp Client ready!"));
+
+        whatsapp.on("message", async message => {
+            if (message.hasMedia) {
+                message.reply("Sorry, media files cannot be handled at this time. Please, send a text-only message.");
+            } else {
+                sendMessage(message)
+                    .then(reply => {
+                        console.log(reply.text);
+                        message.reply(reply.text)
+                    })
+                    .catch(e => {
+                        console.log("Error: ", e.cause);
+                        message.reply("An error was encountered. Please try again in a moment.");
+                    })
+            }
+        })
+
+        whatsapp.initialize();
+    })
